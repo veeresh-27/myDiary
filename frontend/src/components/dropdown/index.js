0e8612f973c79cc52025051039d96dd0a2524e9a
vignesh2718 <vigneshn2718@gmail.com>
2022-08-17 21:08:55 +0530

    Made some changes in Design

--- frontend/src/components/dropdown/index.js
+++ frontend/src/components/dropdown/index.js
@@ -4,10 +4,10 @@ import { ChevronDownIcon } from "@heroicons/react/solid";
 
 export default function Dropdown({ title, options, mode }) {
   return (
-    <div className=" text-right ">
+    <div className="text-right ">
       <Menu as="div" className="relative inline-block text-left ">
-        <div className="flex justify-center items-center">
-          <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-md text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <div className="flex items-center justify-center">
+          <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-white rounded-xl text-md hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75">
             {title}
             <ChevronDownIcon
               className={
@@ -21,21 +21,21 @@ export default function Dropdown({ title, options, mode }) {
         </div>
         <Transition
           as={Fragment}
-          enter="transition ease-out duration-100"
+          enter="transition ease-out duration-100  transform opacity-0"
           enterFrom="transform opacity-0 scale-95"
           enterTo="transform opacity-100 scale-100"
           leave="transition ease-in duration-75"
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95">
-          <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+          <Menu.Items className="absolute right-0 w-48 mt-2 origin-top-right bg-white divide-y divide-gray-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border-cyan-50 rounded-xl">
             <div className="px-1 py-1 bg-white">
               {options?.map(({ name, method }, index) => (
                 <Menu.Item key={index}>
                   {({ active }) => (
                     <button
                       className={`${
-                        active ? "bg-blue-500 text-white" : " text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        active ? "bg-blue-500  text-white  rounded-xl" : " text-gray-900 "
+                      } group flex w-full items-center px-2 py-2 text-sm` }
                       onClick={method ? method : ""}>
                       {name}
                     </button>
--- frontend/src/pages/calendar/index.js
+++ frontend/src/pages/calendar/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return <div className="p-4 text-3xl font-normal sm:text-4xl md:text-5xl lg:text-6 ">Welcome to Calendar</div>;
 };
 
 export default Calendar;
