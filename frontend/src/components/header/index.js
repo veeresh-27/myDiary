2828eccd540a48266393f16bc6b8ed96c23f2a7a
vignesh2718 <vigneshn2718@gmail.com>
2022-08-18 22:10:10 +0530

    Login & Register Page Update

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between w-full bg-background md:px-5 lg:px-14 xl:px-20 h-14 ">
       <div
-        className="p-3 text-3xl text-primary cursor-pointer font-medium sm:text-2xl md:text-3xl lg:text-4xl"
+        className="p-3 text-3xl font-bold cursor-pointer text-primary sm:text-2xl md:text-3xl lg:text-4xl"
         onClick={() => navigate("/")}>
         <h1>My&nbsp;Diary</h1>
       </div>
